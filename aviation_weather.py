90b33d20f5e8de1028ad7de78a1c9e62f5c6cf49
Jeremy Stevens <jeremiahstevens@gmail.com>
2023-04-07 17:44:04 -0700

    update

--- aviation_weather.py
+++ aviation_weather.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import requests
-import metar
+from avwx import Metar, Station
+from avwx.exceptions import InvalidRequest
+from avwx.token import Token
 
 # Define page title and favicon
 st.set_page_config(page_title="My Streamlit Website", page_icon=":airplane:")
@@ -8,9 +9,9 @@ st.set_page_config(page_title="My Streamlit Website", page_icon=":airplane:")
 # Define functions to fetch aviation weather data
 @st.cache
 def fetch_weather_data(airport_code):
-    url = f"https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={airport_code}&hoursBeforeNow=1"
-    response = requests.get(url)
-    return response.content
+    station = Station.from_icao(airport_code)
+    report = Metar(station, Token()).report
+    return report.raw
 
 # Define user interface components
 st.write("""
@@ -19,18 +20,15 @@ st.write("""
 
 airport_code = st.text_input("Enter an airport code:")
 if airport_code:
-    weather_data = fetch_weather_data(airport_code)
+    try:
+        weather_data = fetch_weather_data(airport_code)
 
-    if len(weather_data) > 0:
-        decoded_data = metar.parse(weather_data.decode())
-        st.write(f"""
-                 ## Latest Aviation Weather at {airport_code}
-                 """)
-        st.write(f"Report time: {decoded_data.time}")
-        st.write(f"Station: {decoded_data.station}")
-        st.write(f"Temperature: {decoded_data.temperature_c}°C / {decoded_data.temperature_f}°F")
-        st.write(f"Dew point: {decoded_data.dewpoint_c}°C / {decoded_data.dewpoint_f}°F")
-        st.write(f"Visibility: {decoded_data.visibility_km} km / {decoded_data.visibility_mi} mi")
-        st.write(f"Wind: {decoded_data.wind_speed_kt} kt, direction {decoded_data.wind_direction}")
-    else:
-        st.warning("No weather data found for the given airport code.")
+        if len(weather_data) > 0:
+            st.write(f"""
+                     ## Latest Aviation Weather at {airport_code}
+                     """)
+            st.write(weather_data)
+        else:
+            st.warning("No weather data found for the given airport code.")
+    except InvalidRequest as e:
+        st.warning(str(e))
